a38110c0f167c30f188ecd530f2cae83b0a7d112
Noor <thenoorulhassan.dev@gmail.com>
2026-06-27 23:04:31 +0500

    Db

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
+import dbConnect from "./database/db.js";
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+dbConnect();
 
-app.listen(8080, () => {
-  console.log("Server is listening");
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
 
 app.get("/", (req, res) => {
