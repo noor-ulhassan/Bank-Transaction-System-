99b1d3f8930587d3c02bd89d4374b507a110425e
Noor <thenoorulhassan.dev@gmail.com>
2026-06-28 01:25:20 +0500

    User Model

---
+++ server/src/models/user.model.js
@@ -0,0 +1,48 @@
+import mongoose, { mongo } from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: [true, "Email is Required for creating a user"],
+      trim: true,
+      unique: true,
+      lowercase: true,
+      match: [
+        /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+        "Invalid E-mail Address",
+      ],
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minLength: [8, "Password should contain at-least 8 characters"],
+      select: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
+  return next();
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+const userModel = mongoose.model("User", userSchema);
+
+export default userModel;
